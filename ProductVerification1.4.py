e97d7a62f94cab9756a25f55fbbdff47ad3c50eb
Gleison Brito <brittobaptista93@gmail.com>
2023-10-25 17:07:11 -0300

    mais uma alteracao

--- ProductVerification1.4.py
+++ ProductVerification1.4.py
@@ -7,5 +7,5 @@ def list_of_songs(songslist):
     while songstoplay != []:
         choosed_song = random.randrange(len(songstoplay)) #Randomly choosing some song from the list
         print("Playing:", songstoplay[choosed_song])
-        songstoplay.pop(choosed_song) #Deleting the song from the songstoplay list
+        songstoplay.pop(choosed_song) #alterando coisas
         
